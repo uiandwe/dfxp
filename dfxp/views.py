13a16d1e4de6bb28041e40a9c0a718877997bc38
hyeonsj <hyeonsj@cimple.kr>
2015-12-06 16:36:40 +0900

    모바일 check 추가 (webp 이미지 모바일 불가)

--- dfxp/views.py
+++ dfxp/views.py
@@ -25,7 +25,9 @@ def Index(request):
     for i in range(1, 4):
         image_list.append(random.randrange(1, 175))
 
-    return render(request, 'index.html', {'image_list': image_list})
+    is_mobile = process_request(request)
+
+    return render(request, 'index.html', {'image_list': image_list, 'is_mobile': is_mobile})
 
 
 def error(request):
@@ -83,7 +85,8 @@ def upload(req):
 
             #파일이 <sami>로 시작하지 않으면 에러 페이지로
             if file_data.find('<SAMI>') < 0:
-                return render(req, 'error.html', {'type': 'fake_smi'})
+                if file_data.find('<sami>') < 0:
+                    return render(req, 'error.html', {'type': 'fake_smi'})
             #유니코드로 다시 인코딩
             file_data = file_data.decode('utf-8')
 
@@ -108,3 +111,56 @@ def upload(req):
         return render(req, 'error.html', {'type': 'not_file'})
     #post가 아닌 접근이면 에러 페이지로
     return render(req, 'error.html', {'type': 'get'})
+
+
+def process_request(request):
+        is_mobile = False
+
+        if request.META.has_key('HTTP_USER_AGENT'):
+            user_agent = request.META['HTTP_USER_AGENT']
+
+            # Test common mobile values.
+            pattern = "(up.browser|up.link|mmp|symbian|smartphone|midp|wap|phone|windows ce|pda|mobile|mini|palm|netfront)"
+            prog = re.compile(pattern, re.IGNORECASE)
+            match = prog.search(user_agent)
+
+            if match:
+                is_mobile = True
+            else:
+                # Nokia like test for WAP browsers.
+                # http://www.developershome.com/wap/xhtmlmp/xhtml_mp_tutorial.asp?page=mimeTypesFileExtension
+
+                if request.META.has_key('HTTP_ACCEPT'):
+                    http_accept = request.META['HTTP_ACCEPT']
+
+                    pattern = "application/vnd\.wap\.xhtml\+xml"
+                    prog = re.compile(pattern, re.IGNORECASE)
+
+                    match = prog.search(http_accept)
+
+                    if match:
+                        is_mobile = True
+
+            if not is_mobile:
+                # Now we test the user_agent from a big list.
+                user_agents_test = ("w3c ", "acs-", "alav", "alca", "amoi", "audi",
+                                    "avan", "benq", "bird", "blac", "blaz", "brew",
+                                    "cell", "cldc", "cmd-", "dang", "doco", "eric",
+                                    "hipt", "inno", "ipaq", "java", "jigs", "kddi",
+                                    "keji", "leno", "lg-c", "lg-d", "lg-g", "lge-",
+                                    "maui", "maxo", "midp", "mits", "mmef", "mobi",
+                                    "mot-", "moto", "mwbp", "nec-", "newt", "noki",
+                                    "xda",  "palm", "pana", "pant", "phil", "play",
+                                    "port", "prox", "qwap", "sage", "sams", "sany",
+                                    "sch-", "sec-", "send", "seri", "sgh-", "shar",
+                                    "sie-", "siem", "smal", "smar", "sony", "sph-",
+                                    "symb", "t-mo", "teli", "tim-", "tosh", "tsm-",
+                                    "upg1", "upsi", "vk-v", "voda", "wap-", "wapa",
+                                    "wapi", "wapp", "wapr", "webc", "winw", "winw",
+                                    "xda-",)
+
+                test = user_agent[0:4].lower()
+                if test in user_agents_test:
+                    is_mobile = True
+
+        return is_mobile
